d75952f3a283eb715f4e44c581749896ecb6f2ee
Clara <clarafdezfraga@gmail.com>
2024-06-05 17:36:29 +0200

    Add mockStore to galleryPage test

--- src/pages/GalleryPage.test.tsx
+++ src/pages/GalleryPage.test.tsx
@@ -2,6 +2,10 @@ import { Provider } from "react-redux";
 import { store } from "../store/store";
 import GalleryPage from "./GalleryPage";
 import { render, screen } from "@testing-library/react";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { ArtworksState } from "../artworks/artworksSlice/types";
+
+import { mockMonaLisa } from "../artworks/mocks/artworks";
 
 describe("given a GalleryPage component", () => {
   describe("When the artworks list is empty", () => {
@@ -23,8 +27,23 @@ describe("given a GalleryPage component", () => {
 
   describe("When the artworks list contains 'la mona Lisa'", () => {
     test("Then it should show a heading with the text 'la mona Lisa'", () => {
+      const initialState: ArtworksState = {
+        artworks: [mockMonaLisa],
+      };
+      const mockSlice = createSlice({
+        name: "artworks",
+        initialState,
+        reducers: {},
+      });
+
+      const mockStore = configureStore({
+        reducer: {
+          artworks: mockSlice.reducer,
+        },
+      });
+
       render(
-        <Provider store={store}>
+        <Provider store={mockStore}>
           <GalleryPage />
         </Provider>,
       );
