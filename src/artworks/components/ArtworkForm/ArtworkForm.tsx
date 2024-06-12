d7f1b9003bf8b817072e53246d920f7b09571aa3
Clara <clarafdezfraga@gmail.com>
2024-06-12 16:31:27 +0200

    Add state to form

--- src/artworks/components/ArtworkForm/ArtworkForm.tsx
+++ src/artworks/components/ArtworkForm/ArtworkForm.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { NewArtworkData } from "../../types";
 import "./ArtworkForm.scss";
 
 const ArtworkForm = (): React.ReactElement => {
+  const [newArtworkData, setNewArtworkData] = useState<NewArtworkData>({
+    title: "",
+    author: "",
+    location: "",
+    description: "",
+    artworkUrl: "",
+    medium: "",
+    year: 2024,
+    height: 0,
+    width: 0,
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) =>
+    setNewArtworkData((previousNewArtworkData) => {
+      return {
+        ...previousNewArtworkData,
+        [event.target.id]: event.target.value,
+      };
+    });
   return (
     <form
       className="form"
@@ -11,15 +33,35 @@ const ArtworkForm = (): React.ReactElement => {
     >
       <div className="form__group">
         <label htmlFor="author">Autor: </label>
-        <input className="form__input" id="author" type="text" />
+        <input
+          className="form__input"
+          id="author"
+          type="text"
+          onChange={handleChange}
+          value={newArtworkData.author}
+        />
       </div>
       <div className="form__group">
         <label htmlFor="title">Título: </label>
-        <input required className="form__input" id="title" type="text" />
+        <input
+          required
+          className="form__input"
+          id="title"
+          type="text"
+          onChange={handleChange}
+          value={newArtworkData.title}
+        />
       </div>
       <div className="form__group">
         <label htmlFor="location">Ubicación: </label>
-        <input required className="form__input" id="location" type="text" />
+        <input
+          required
+          className="form__input"
+          id="location"
+          type="text"
+          onChange={handleChange}
+          value={newArtworkData.location}
+        />
       </div>
       <span>Dimensiones:</span>
       <div className="size">
@@ -34,6 +76,8 @@ const ArtworkForm = (): React.ReactElement => {
               className="form__input form__input--size"
               id="height"
               type="number"
+              onChange={handleChange}
+              value={newArtworkData.height}
             />
             <span> cm</span>
           </div>
@@ -43,6 +87,8 @@ const ArtworkForm = (): React.ReactElement => {
               className="form__input form__input--size"
               id="width"
               type="number"
+              onChange={handleChange}
+              value={newArtworkData.width}
             />
             <span> cm</span>
           </div>
@@ -50,11 +96,25 @@ const ArtworkForm = (): React.ReactElement => {
       </div>
       <div className="form__group">
         <label htmlFor="medium">Técnica: </label>
-        <input required className="form__input" id="medium" type="text" />
+        <input
+          required
+          className="form__input"
+          id="medium"
+          type="text"
+          onChange={handleChange}
+          value={newArtworkData.medium}
+        />
       </div>
       <div className="form__group">
-        <label htmlFor="rtworkUrl">Url de la Obra: </label>
-        <input required className="form__input" id="rtworkUrl" type="url" />
+        <label htmlFor="artworkUrl">Url de la Obra: </label>
+        <input
+          required
+          className="form__input"
+          id="artworkUrl"
+          type="url"
+          onChange={handleChange}
+          value={newArtworkData.artworkUrl}
+        />
       </div>
       <div className="form__group">
         <label htmlFor="description">Descripción: </label>
@@ -62,11 +122,20 @@ const ArtworkForm = (): React.ReactElement => {
           required
           className="form__input form__input--textarea"
           id="description"
+          onChange={handleChange}
+          value={newArtworkData.description}
         />
       </div>
       <div className="form__group">
         <label htmlFor="year">Año de creación:</label>
-        <input required className="form__input" id="year" type="number" />
+        <input
+          required
+          className="form__input"
+          id="year"
+          type="number"
+          onChange={handleChange}
+          value={newArtworkData.year}
+        />
       </div>
       <button type="submit" className="form__button">
         GUARDAR
